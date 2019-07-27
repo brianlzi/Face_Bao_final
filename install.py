eb5294bab182a6263bb5541435914194dbdd0d08
Nguyen Van Giang <44034128+gnvml@users.noreply.github.com>
2019-07-27 22:15:59 +0700

    Update install.py

--- install.py
+++ install.py
@@ -2,6 +2,6 @@ import os
 
 os.system("pip install -r requirements.txt")
 os.system("conda install -c conda-forge opencv")
-os.system("cd .. && git clone https://github.com/tzutalin/labelImg.git")
+os.system("git clone https://github.com/tzutalin/labelImg.git")
 os.system("conda install pyqt=5")
 os.system("cd labelImg && pyrcc5 -o libs/resources.py resources.qrc")
