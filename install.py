f32812630a732ea34fd2b621ddd2b7a27b9671d3
Your Name <you@example.com>
2019-07-31 15:41:04 +0700

    add icon

--- install.py
+++ install.py
@@ -1,7 +1,9 @@
 import os
 
-os.system("pip install -r requirements.txt")
-os.system("conda install -c conda-forge opencv")
-os.system("git clone https://github.com/tzutalin/labelImg.git")
-os.system("conda install pyqt=5")
-os.system("cd labelImg && pyrcc5 -o libs/resources.py resources.qrc")
+# os.system("pip install -r requirements.txt")
+# os.system("conda install -c conda-forge opencv")
+# os.system("git clone https://github.com/tzutalin/labelImg.git")
+# os.system("conda install pyqt=5")
+# os.system("cd labelImg && pyrcc5 -o libs/resources.py resources.qrc")
+os.system("cp -i /home/udth/Face_Bao/Bao.desktop /home/udth/Desktop")
+os.system("chmod a+x /home/udth/Desktop/Bao.desktop")
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec $(cd /home/udth/Face_Bao && python /home/udth/Face_Bao/app.py)
